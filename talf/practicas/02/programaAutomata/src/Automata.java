01309b27609a289c58656a0bb2f5a2a5433de4bc
fm <fernandomosteiro30@gmail.com>
2019-10-14 19:40:07 +0200

    TALF - init practica 2

---
+++ talf/practicas/02/programaAutomata/src/Automata.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Automata {
+    private Estado[] estados;
+    private Estado estadoInicial;
+    private HashSet<Estado> estadosFinales;
+
+    private Estado[][] matrizTransiciones;
+
+    private HashSet<String> alfabeto;
+
+    /* ----------------------------------------------------------------- */
+
+}
