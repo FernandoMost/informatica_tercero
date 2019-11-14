bcad9beeda9065c693d8f82dd0195d678fec260d
fm <fernandomosteiro30@gmail.com>
2019-11-14 20:36:25 +0100

    asr

---
+++ asr/practicas/06/tres.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# VOLVER AL BACKUP
+
+departamentos=(gerencia administracion programacion sistemas comercial rrhh contabilidad)
+
+for d in "${departamentos[@]}"; do
+  # echo "$d"
+  # CREAR GRUPOS PARA CADA DEPARTAMENTO
+done
+
+# CREAR GRUPOS PARA JEFES Y EMPLEADOS
+
+
+
+for d in "${departamentos[@]}"; do
+  # MKDIR
+  # CREAR DIRECTORIOS PARA CADA DEPARTAMENTO
+  # dentro de cada uno, crear ./work
+done
+
+
+
+# directorios de nuevos usuarios se crearán en estos directorios
+# solo pueden entrar ese directorio (ver .txt)
+
+# work es comun en el departamento
+
+
+
+
+# CREAR DIRECTORIO COMUN
+# (tods pueden acceder y crear, sólo root puede borrar)
+# dentro /comunJefes, exclusivo para jefes
+# sus ficheros solo pueden ser borrados por el creador y root
+
+
+
