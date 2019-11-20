e081f76986bbd38316af7510706424b6df77f597
FM <fernandomosteiro30@gmail.com>
2019-11-20 15:16:08 +0100

    ASR - more 06

---
+++ asr/practicas/06/cuatro.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+import pwd
+import random
+import string
+import subprocess
+import crypt
+
+parser = argparse.ArgumentParser(description="Creación de usuarios en python")
+
+parser.add_argument("-u", dest="usuario", required=True, help="Nombre del usuario a crear")
+parser.add_argument("-d", dest="departamento", required=True, help="Departamento al que pertenece el usuario")
+
+# true = jefe; false = empleado
+grupo = parser.add_mutually_exclusive_group()
+grupo.add_argument("-j", dest="esJefe", action='store_true', help="El usuario es de nivel jefe")
+grupo.add_argument("-e", dest="esJefe", action='store_false', help="El usuario es de nivel empleado")
+
+parser.add_argument("-p", dest="clave", help="Contraseña de acceso para el usuario, si falta se creará una aleatoria")
+
+args = parser.parse_args()
+
+# ──────────────────────────────────────────────────────────────────
+
+try:
+    pwd.getpwnam(args.usuario)
+    print("Error. El usuario " + args.usuario + " existe")
+except KeyError:
+    # detectar que el departamento es válido
+
+    if args.clave is None:
+        alfabeto = string.ascii_letters + string.digits
+        args.clave = ''.join(random.choice(alfabeto) for i in range(8))
+
+    encriptada = crypt.crypt(args.clave, "sementar_sementarei")
+
+    # directorio /home/{departamento}/{usuario}
+    directorio = "/home/" + args.departamento + "/" + args.usuario
+
+    if args.esJefe:
+        nivel = "jefes"
+    else:
+        nivel = "empleados"
+
+    gecos = args.usuario + ", " + args.departamento
+
+    # crear usuario, usuario a grupo, usuario a jefe/empleado
+    subprocess.call(['useradd', args.usuario, '-p', encriptada, '-d', directorio, '-g', args.departamento, '-G', nivel, '-c', gecos, '-s', '/bin/bash'])
+    subprocess.call(['passwd', '-x 30', '-w 10', args.usuario])
+
+    # no acaba de funcionar el directorio!
+
+    # ln -s /home/{departamento}/work /home/{departamento}/{usuario}/comunDepartamento
+    # ln -s /home/comun /home/{departamento}/{usuario}/comun
+    # IF ... ln -s /home/comun/comunJefes /home/{departamento}/{usuario}/comuinJefes
+
+    print("Usuario creado satisfactoriamente")
+    print("user:\t" + args.usuario)
+    print("dep:\t" + args.departamento)
+    print("clave:\t" + args.clave)
+    # print("home:\t")
+    # print("gecko:\t")
+    # print("grupo:\t")
+    # print("opciones de la contrasena puestas correctamente")
+    # print("creado:\t")
+    # print("creado:\t")
--- asr/practicas/06/tres.sh
+++ asr/practicas/06/tres.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 
-# VOLVER AL BACKUP
-rm -rf /home/gerencia
-rm -rf /home/administracion
-rm -rf /home/programacion
-rm -rf /home/sistemas
-rm -rf /home/comercial
-rm -rf /home/rrhh
-rm -rf /home/contabilidad
-
+# conjunto con los departamentos
 departamentos=(gerencia administracion programacion sistemas comercial rrhh contabilidad)
 
-for d in "${departamentos[@]}"; do
-  # echo "$d"
-  # CREAR GRUPOS PARA CADA DEPARTAMENTO
+for d in "${departamentos[@]}"; do    # iterando por los departamentos
+  # creando el grupo del departamento
   groupadd "$d"
 
-  # CREAR DIRECTORIOS PARA CADA DEPARTAMENTO
-  # dentro de cada uno, crear ./work
+  # directorio para el departamento, dentro de /home
+  # dentro del mismo, directorio work
+  # con la opción -p de mkdir creamos ambos en un sólo comando
   mkdir -pv /home/"$d"/work
 
   # directorios de nuevos usuarios se crearán en estos directorios
   # solo pueden entrar ese directorio (ver .txt)
-  chmod o-rwx /home/"$d"/
+
+  # quitamos todos los permisos de Others en /home/departamento
+  chmod -v o-rwx /home/"$d"/
+
+  # todos los permisos de propietario y grupo en /home/work
   chmod -v 770 /home/"$d"/work
-  chown -v :"$d" /home/"$d"/
+
+  # se cambia de propietario todos los contenidos de manera recursiva
+  # de /home/departamento a todos los usuarios del grupo correspondiente
+  chgrp -Rv "$d" /home/"$d"/
+
+  # barra estética
+  echo "───────────────────────────────────────────"
 done
 
-# CREAR GRUPOS PARA JEFES Y EMPLEADOS
+# los dos grupos adicionales
 groupadd jefes
 groupadd empleados
 
-
-#sgid
-#chmod acá abajo
-
-# CREAR DIRECTORIO COMUN
+# directorio común y comúnJefes dentro de este
+# con la opción -p de mkdir creamos ambos en un sólo comando
 mkdir -pv /home/comun/comunJefes
-chmod -v +t /home/comun 
 
-# dentro /comunJefes, exclusivo para jefes
-chmod o-rwx /home/comun/comunJefes
-chown -v :jefes /home/comun/comunJefes
+# todos los permisos en el directorio común
+chmod -v 777 /home/comun/
 
-# (tods pueden acceder y crear, sólo root puede borrar)
-# sus ficheros solo pueden ser borrados por el creador y root
-chmod -v +t /home/comun/comunJefes
+# sin los permisos para otros en el directorio comun para jefes
+chmod -v 770 /home/comun/comunJefes
+
+# cambiando el propietario de /comunJefes al grupo de jefes
+chgrp -v jefes /home/comun/comunJefes
+
+# cambiando el sticky bit en /comun y /comunJefes
+# así solo pueden ser eliminados por su creador o root
+chmod -Rv +t /home/comun/
