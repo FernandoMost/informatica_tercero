82ff8d4e9670e0ae131be486f9c40cdda9c50120
FM <fernandomosteiro30@gmail.com>
2019-11-21 17:16:50 +0100

    IPO - MÁS TEATRO

--- ipo/practicas/04/implementacion/src/sample/Controller.java
+++ ipo/practicas/04/implementacion/src/sample/Controller.java
@@ -1,4 +1,65 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+
 public class Controller {
+
+    Timeline sreenSaver;
+    private ArrayList<ImageView> imagesScreenSaver;
+    private int indexActualImage;
+
+    @FXML private ImageView imageScreenSaver1;
+    @FXML private ImageView imageScreenSaver2;
+    @FXML private ImageView imageScreenSaver3;
+    @FXML private ImageView imageScreenSaver4;
+
+    //───────────────────────────────────────
+
+    @FXML
+    public void initialize() {
+        imagesScreenSaver = new ArrayList<>();
+
+        imagesScreenSaver.add(imageScreenSaver1);
+        imagesScreenSaver.add(imageScreenSaver2);
+        imagesScreenSaver.add(imageScreenSaver3);
+        imagesScreenSaver.add(imageScreenSaver4);
+
+        indexActualImage = 0;
+
+        for (ImageView im : imagesScreenSaver) im.setVisible(false);
+
+        sreenSaver = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            indexActualImage++;
+            indexActualImage = indexActualImage % imagesScreenSaver.size();
+
+            for (ImageView im : imagesScreenSaver) im.setVisible(false);
+
+            imagesScreenSaver.get(indexActualImage).setVisible(true);
+        }));
+
+        sreenSaver.setCycleCount(Timeline.INDEFINITE);
+
+        arrancaScreenSaver();
+    }
+
+    // ───────────────────────────────────────────────────────────────────────────────────────────
+
+    private void arrancaScreenSaver() {
+        sreenSaver.play();
+    }
+
+    private void paraScreenSaver() {
+        sreenSaver.stop();
+        for (ImageView im : imagesScreenSaver) im.setVisible(false);
+    }
+
+    public void clickadoprueba() {
+        paraScreenSaver();
+    }
 }
--- ipo/practicas/04/implementacion/src/sample/Main.java
+++ ipo/practicas/04/implementacion/src/sample/Main.java
@@ -2,8 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,11 +13,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setTitle("Quiosco teatro");
+
+        primaryStage.setScene(new Scene(root, 700, 1000));
+
+        primaryStage.setResizable(false );
+
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
