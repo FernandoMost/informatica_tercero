811a877528f6387b24c7d68204b894db492fda4f
FM <fernandomosteiro30@gmail.com>
2019-12-11 02:16:17 +0100

    daw - that's sql

---
+++ daw/VizualContender/sql/vaciadoBorrado.sql
@@ -0,0 +1,29 @@
+USE bd_alumnos;
+DELETE FROM altadaw;
+
+USE mosteiroDelPilar;
+DELETE FROM articuloEnPedido;
+DELETE FROM pedido;
+DELETE FROM carrito;
+DELETE FROM articulo;
+DELETE FROM direccionEnvio;
+DELETE FROM metodoPago;
+DELETE FROM usuario;
+
+
+
+
+
+
+
+USE bd_alumnos;
+DROP TABLE altadaw;
+
+USE mosteiroDelPilar;
+DROP TABLE articuloEnPedido;
+DROP TABLE pedido;
+DROP TABLE carrito;
+DROP TABLE articulo;
+DROP TABLE direccionEnvio;
+DROP TABLE metodoPago;
+DROP TABLE usuario;
