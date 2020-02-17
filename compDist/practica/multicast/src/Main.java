10a9643758f484c561e9123658ab69348e3269d9
fm <fernandomosteiro30@gmail.com>
2020-02-17 14:30:31 +0100

    init Distribuida y lil enso

---
+++ compDist/practica/multicast/src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("graphics/sample.fxml"));
+        primaryStage.setTitle("Práctica Multicast");
+        // primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
