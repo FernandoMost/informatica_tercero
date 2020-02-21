7c1e7e66a3308a66732e4ee4de91409bf7173f71
FM <fernandomosteiro30@gmail.com>
2020-02-21 08:44:05 +0100

    multicast

--- compDist/practica/multicast/src/Main.java
+++ compDist/practica/multicast/src/Main.java
@@ -3,16 +3,62 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("graphics/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("graphics/login.fxml"));
         primaryStage.setTitle("Práctica Multicast");
-        // primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 413, 397));
         primaryStage.show();
+
+
+
+
+
+
+
+
+
+
+        /*
+        *
+        * import java.net.*;
+        import java.io.*;
+        public class MulticastPeer{
+        public static void main(String args[]){
+        // args give message contents & destination multicast group (e.g. "228.5.6.7")
+        MulticastSocket s =null;
+        try {
+        InetAddress group = InetAddress.getByName(args[1]);
+        s = new MulticastSocket(6789);
+        s.joinGroup(group);
+        byte [] m = args[0].getBytes();
+        DatagramPacket messageOut =
+        new DatagramPacket(m, m.length, group, 6789);
+        s.send(messageOut);
+        // this figure continued on the next slide
+        Distributed computing
+        33// get messages from others in group
+        byte[] buffer = new byte[1000];
+        for(int i=0; i< 3; i++) {
+        DatagramPacket messageIn =
+        new DatagramPacket(buffer, buffer.length);
+        s.receive(messageIn);
+        System.out.println("Received:" + new String(messageIn.getData()));
+        }
+        s.leaveGroup(group);
+        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e){System.out.println("IO: " + e.getMessage());}
+        }finally {if(s != null) s.close();}
+        }
+        }
+        *
+        *
+        * */
     }
 
 
--- compDist/practica/multicast/src/graphics/Controller.java
+++ compDist/practica/multicast/src/graphics/Controller.java
@@ -1,4 +1,49 @@
 package graphics;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML private JFXButton minimizarButton;
+    @FXML private JFXButton cerrarButton;
+    @FXML private HBox topBar;
+    @FXML private BorderPane primaryStageLogin;
+
+    private double xOffset, yOffset;
+
+    // ───────────────────────────────────────────────────────────────────────────────────────────
+
+    @FXML public void initialize() {
+    }
+
+    public void minimizar(MouseEvent e) {
+        ((Stage) ((Button) e.getSource()).getScene().getWindow()).setIconified(true);
+    }
+
+    public void cerrar() {
+        System.exit(0);
+    }
+
+    // ───────────────────────────────────────────────────────────────────────────────────────────
+
+    public void takeWindowOffset(MouseEvent e) {
+        Stage primaryStage = (Stage) ((HBox) e.getSource()).getScene().getWindow();
+
+        xOffset = primaryStage.getX() - e.getScreenX();
+        yOffset = primaryStage.getY() - e.getScreenY();
+    }
+
+    public void moveWindow(MouseEvent e) {
+        Stage primaryStage = (Stage) ((HBox) e.getSource()).getScene().getWindow();
+
+        primaryStage.setX(e.getScreenX() + xOffset);
+        primaryStage.setY(e.getScreenY() + yOffset);
+    }
 }
