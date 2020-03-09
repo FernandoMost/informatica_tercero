e536b45b6d97a97b35192d220fa1518cbfd4a0da
fm <fernandomosteiro30@gmail.com>
2020-03-09 14:27:36 +0100

    dist

--- compDist/practica/2/montecarlo/src/MontecarloClient.java
+++ compDist/practica/2/montecarlo/src/MontecarloClient.java
@@ -1,6 +1,25 @@
 import java.rmi.Naming;
 
 public class MontecarloClient {
+    static int[] laCumplen;
+
+    public static class MontecarloClientThread extends Thread {
+        MontecarloInterface montecarloInterface;
+        int iteraciones;
+
+        MontecarloClientThread(MontecarloInterface montecarloInterface, int iteraciones) {
+            this.montecarloInterface = montecarloInterface;
+            this.iteraciones = iteraciones;
+        }
+
+        public void run() {
+            try {
+                laCumplen[Integer.parseInt(this.getName())] = montecarloInterface.doTheMonthecarloMethod(iteraciones);
+            } catch (Exception e) {
+                System.out.println("Exception in MontecarloClient: " + e);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         try {
@@ -15,16 +34,23 @@ public class MontecarloClient {
             String portNum = "1099";
             String registryURL = "rmi://" + hostName+ ":" + portNum + "/montecarlo";
 
-            MontecarloInterface h = (MontecarloInterface) Naming.lookup(registryURL);
+            final int NUM_HILOS = 4;
+            final int NUM_ITERACIONES = 1000000;
+
+            laCumplen = new int[NUM_HILOS];
+
+            MontecarloInterface anInterface = (MontecarloInterface) Naming.lookup(registryURL);
             System.out.println("Lookup completed " );
 
-            int m = h.doTheMonthecarloMethod(1000000);
-            double piCuartos = (double) m / 1000000;
+            for (int i = 0; i < NUM_HILOS; i++) {
+                MontecarloClientThread hilo = new MontecarloClientThread(anInterface, NUM_ITERACIONES);
+                hilo.setName(String.valueOf(i));
+                hilo.start();
+            }
 
 
-            System.out.println("MontecarloClient: " + piCuartos*4.0);
         } catch (Exception e) {
-            System.out.println("Exception in HelloClient: " + e);
+            System.out.println("Exception in MontecarloClient: " + e);
         }
     }
 }
