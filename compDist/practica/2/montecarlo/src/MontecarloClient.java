4422c847cbe40f60172ede20aa2533be1a54fbca
FM <fernandomosteiro30@gmail.com>
2020-03-10 01:39:21 +0100

    dist

--- compDist/practica/2/montecarlo/src/MontecarloClient.java
+++ compDist/practica/2/montecarlo/src/MontecarloClient.java
@@ -1,7 +1,8 @@
 import java.rmi.Naming;
+import java.util.concurrent.Callable;
 
 public class MontecarloClient {
-    static int[] laCumplen;
+    private static int[] laCumplen;
 
     public static class MontecarloClientThread extends Thread {
         MontecarloInterface montecarloInterface;
@@ -12,6 +13,7 @@ public class MontecarloClient {
             this.iteraciones = iteraciones;
         }
 
+        @Override
         public void run() {
             try {
                 laCumplen[Integer.parseInt(this.getName())] = montecarloInterface.doTheMonthecarloMethod(iteraciones);
@@ -34,21 +36,36 @@ public class MontecarloClient {
             String portNum = "1099";
             String registryURL = "rmi://" + hostName+ ":" + portNum + "/montecarlo";
 
-            final int NUM_HILOS = 4;
-            final int NUM_ITERACIONES = 1000000;
+            final int NUM_HILOS = 8;
+            final int NUM_ITERACIONES = 50000000;
 
             laCumplen = new int[NUM_HILOS];
+            MontecarloClientThread[] hilos = new MontecarloClientThread[NUM_HILOS];
 
             MontecarloInterface anInterface = (MontecarloInterface) Naming.lookup(registryURL);
             System.out.println("Lookup completed " );
 
             for (int i = 0; i < NUM_HILOS; i++) {
-                MontecarloClientThread hilo = new MontecarloClientThread(anInterface, NUM_ITERACIONES);
-                hilo.setName(String.valueOf(i));
-                hilo.start();
+                hilos[i] = new MontecarloClientThread(anInterface, NUM_ITERACIONES);
+                hilos[i].setName(String.valueOf(i));
+                hilos[i].start();
             }
 
+            for (int i = 0; i < NUM_HILOS; i++)
+                hilos[i].join();
 
+            int totalLaCumplen = 0;
+            double totalIteraciones = NUM_HILOS*NUM_ITERACIONES;
+
+            for (int valor : laCumplen)
+                totalLaCumplen += valor;
+
+            System.out.println("la cumplen = " + totalLaCumplen);
+            System.out.println("iteraciones totales = " + totalIteraciones);
+
+            double piCuartos = (double)totalLaCumplen / totalIteraciones;
+
+            System.out.println("Pi = " + piCuartos*4.0);
         } catch (Exception e) {
             System.out.println("Exception in MontecarloClient: " + e);
         }
