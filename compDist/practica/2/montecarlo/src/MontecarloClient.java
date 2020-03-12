76aeb5a3e6cb56dbbfbd55d31bfa3549d60fe6f7
FM <fernandomosteiro30@gmail.com>
2020-03-12 09:09:04 +0100

    dist

--- compDist/practica/2/montecarlo/src/MontecarloClient.java
+++ compDist/practica/2/montecarlo/src/MontecarloClient.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.rmi.ConnectException;
 import java.rmi.Naming;
 
 public class MontecarloClient {
@@ -31,40 +32,47 @@ public class MontecarloClient {
             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            System.out.println("Enter the RMIRegistry host namer:");
+            System.out.print("RMIRegistry host name: ");
             hostName = bufferedReader.readLine();
 
-            System.out.println("Enter the RMIregistry port number:");
+            System.out.print("RMIregistry port number: ");
             portNumber = bufferedReader.readLine();
 
             String registryURL = "rmi://" + hostName+ ":" + portNumber + "/montecarlo";
+            MontecarloInterface anInterface = null;
 
-            System.out.println("Enter the number of threads:");
+            try {
+                anInterface = (MontecarloInterface) Naming.lookup(registryURL);
+                System.out.println("Lookup completed!");
+            } catch (ConnectException e) {
+                System.out.println("Conection refused: " + registryURL + "\tEnding the process...");
+                System.exit(-1);
+            }
+
+            System.out.print("Number of threads: ");
             threadNumber = bufferedReader.readLine();
 
-            System.out.println("Enter the number of iterations per thread:");
+            System.out.print("Number of iterations per thread: ");
             iterationsNumber = bufferedReader.readLine();
 
-            final int NUM_HILOS = 8;
-            final int NUM_ITERACIONES = 50000000;
+            int num_hilos = Integer.parseInt(threadNumber), num_iteraciones = Integer.parseInt(iterationsNumber);
 
-            laCumplen = new int[NUM_HILOS];
-            MontecarloClientThread[] hilos = new MontecarloClientThread[NUM_HILOS];
+            laCumplen = new int[num_hilos];
+            MontecarloClientThread[] hilos = new MontecarloClientThread[num_hilos];
 
-            MontecarloInterface anInterface = (MontecarloInterface) Naming.lookup(registryURL);
-            System.out.println("Lookup completed " );
+            long startTime = System.nanoTime();
 
-            for (int i = 0; i < NUM_HILOS; i++) {
-                hilos[i] = new MontecarloClientThread(anInterface, NUM_ITERACIONES);
+            for (int i = 0; i < num_hilos; i++) {
+                hilos[i] = new MontecarloClientThread(anInterface, num_iteraciones);
                 hilos[i].setName(String.valueOf(i));
                 hilos[i].start();
             }
 
-            for (int i = 0; i < NUM_HILOS; i++)
+            for (int i = 0; i < num_hilos; i++)
                 hilos[i].join();
 
             int totalLaCumplen = 0;
-            double totalIteraciones = NUM_HILOS*NUM_ITERACIONES;
+            double totalIteraciones = num_hilos*num_iteraciones;
 
             for (int valor : laCumplen)
                 totalLaCumplen += valor;
@@ -75,6 +83,11 @@ public class MontecarloClient {
             double piCuartos = (double)totalLaCumplen / totalIteraciones;
 
             System.out.println("Pi = " + piCuartos*4.0);
+
+            long endTime = System.nanoTime();
+
+            double seconds = (double)(endTime - startTime) / 1_000_000_000.0;
+            System.out.println("Execution time in seconds  : " + seconds);
         } catch (Exception e) {
             System.out.println("Exception in MontecarloClient: " + e);
         }
